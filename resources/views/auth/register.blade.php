3e51cc69e17003bd987b10b7478a639f820214b0
marthin salakory <marthinsalakory11@gmail.com>
2021-11-04 20:40:14 +0900

    modifikasi tampilan register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,66 +11,135 @@
         <div class="card">
             <div class="card-body register-card-body">
                 <p class="login-box-msg">Register a new membership</p>
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a class="nav-link active" href="#pekerja" data-toggle="tab">Worker</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#perusahaan" data-toggle="tab">Company</a></li>
+                </ul>
 
-                <form action="{{ route('register') }}" method="post">
-                    @csrf
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Full name" name="name">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-user"></span>
+                <div class="tab-content mt-3">
+                    <div class="tab-pane active" id="pekerja">
+                        <form action="{{ route('register') }}" method="post">
+                            @csrf
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Full name" name="name">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-user"></span>
+                                    </div>
+                                </div>
+                                @error('name')
+                                <span class="error invalid-feedback">{{ $message }}</span>
+                                @enderror
                             </div>
-                        </div>
-                        @error('name')
-                        <span class="error invalid-feedback">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
+                            <div class="input-group mb-3">
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-envelope"></span>
+                                    </div>
+                                </div>
+                                @error('email')
+                                <span class="error invalid-feedback">{{ $message }}</span>
+                                @enderror
                             </div>
-                        </div>
-                        @error('email')
-                        <span class="error invalid-feedback">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
+                            <div class="input-group mb-3">
+                                <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-lock"></span>
+                                    </div>
+                                </div>
+                                @error('password')
+                                <span class="error invalid-feedback">{{ $message }}</span>
+                                @enderror
                             </div>
-                        </div>
-                        @error('password')
-                        <span class="error invalid-feedback">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Retype password" name="password_confirmation">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
+                            <div class="input-group mb-3">
+                                <input type="password" class="form-control" placeholder="Retype password" name="password_confirmation">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-lock"></span>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-                                <label for="agreeTerms">
-                                    I agree to the <a href="#">terms</a>
-                                </label>
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="icheck-primary">
+                                        <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+                                        <label for="agreeTerms">
+                                            I agree to the <a href="#">terms</a>
+                                        </label>
+                                    </div>
+                                </div>
+                                <!-- /.col -->
+                                <div class="col-4">
+                                    <button type="submit" class="btn btn-primary btn-block">Register</button>
+                                </div>
+                                <!-- /.col -->
                             </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Register</button>
-                        </div>
-                        <!-- /.col -->
+                        </form>
                     </div>
-                </form>
+                    <div class="tab-pane" id="perusahaan">
+                        <form action="{{ route('register') }}" method="post">
+                            @csrf
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Company name" name="name">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-user"></span>
+                                    </div>
+                                </div>
+                                @error('name')
+                                <span class="error invalid-feedback">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-envelope"></span>
+                                    </div>
+                                </div>
+                                @error('email')
+                                <span class="error invalid-feedback">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-lock"></span>
+                                    </div>
+                                </div>
+                                @error('password')
+                                <span class="error invalid-feedback">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="password" class="form-control" placeholder="Retype password" name="password_confirmation">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-lock"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="icheck-primary">
+                                        <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+                                        <label for="agreeTerms">
+                                            I agree to the <a href="#">terms</a>
+                                        </label>
+                                    </div>
+                                </div>
+                                <!-- /.col -->
+                                <div class="col-4">
+                                    <button type="submit" class="btn btn-primary btn-block">Register</button>
+                                </div>
+                                <!-- /.col -->
+                            </div>
+                        </form>
+                    </div>
+                </div>
 
                 {{-- <div class="social-auth-links text-center">
                     <p>- OR -</p>
@@ -91,12 +160,5 @@
         <!-- /.card -->
     </div>
     <!-- /.register-box -->
-
-    <!-- jQuery -->
-    <script src="../../plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="../../dist/js/adminlte.min.js"></script>
 </body>
 @endsection
