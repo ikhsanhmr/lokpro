9e1fe41be5cce089c1e6900cae22ce5ad9ac3bfe
marthin salakory <marthinsalakory11@gmail.com>
2021-11-06 11:18:05 +0900

    menambahkan 1 field yang terlewat pada helper

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -8,7 +8,7 @@ function user()
     // mengambil data users dan users
     return DB::table('data_users')
         ->join('users', 'users.id', '=', 'data_users.user_id')
-        ->select('users.*', 'data_users.id as data_users_id', 'data_users.job_location', 'data_users.company_location', 'data_users.company_culture', 'data_users.sosmed')
+        ->select('users.*', 'data_users.id as data_users_id', 'data_users.job_location', 'data_users.jumlah_pekerja', 'data_users.company_location', 'data_users.company_culture', 'data_users.sosmed')
         ->where('data_users.user_id', '=', Auth::user()->id)
         ->first();
     // return Auth::user();
