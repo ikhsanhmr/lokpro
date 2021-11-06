d38aecf71ffb1a323bf5f9d40ddae71749a183da
fadlie ferdiyansah <fadlieferdiyansah26@gmail.com>
2021-11-06 21:03:09 +0700

    hapus code yang tak terpakai

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,6 @@
-@extends('auth.layouts.app')
+@extends('auth.layouts.app', ['title' => 'Daftar Jobseeker dan Company - '])
 
 @section('content')
-
 <body class="hold-transition register-page">
     <div class="register-box">
         <div class="register-logo">
@@ -109,12 +108,5 @@
         <!-- /.card -->
     </div>
     <!-- /.register-box -->
-
-    <!-- jQuery -->
-    <script src="../../plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="../../dist/js/adminlte.min.js"></script>
 </body>
 @endsection
