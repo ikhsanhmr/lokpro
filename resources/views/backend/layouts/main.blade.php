63246534af90f717ad78f3e37522d2772a0740e0
marthin salakory <marthinsalakory11@gmail.com>
2021-11-11 21:54:21 +0900

    menambahkan select active sidebar

--- resources/views/backend/layouts/main.blade.php
+++ resources/views/backend/layouts/main.blade.php
@@ -1,3 +1,4 @@
+<?php $url = explode('/', url()->full()) ?>
 <!DOCTYPE html>
 <html lang="en">
 
--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -13,7 +13,7 @@
     <ul class="menu">
         <li class="sidebar-title">Menu</li>
 
-        <li class="sidebar-item active ">
+        <li class="sidebar-item {{ end($url)=='dashboard'?'active':'' }}">
             <a href="/{{ user()->role }}/dashboard" class='sidebar-link'>
                 <i class="bi bi-grid-fill"></i>
                 <span>Dashboard</span>
@@ -27,23 +27,23 @@
             </a>
         </li> --}}
         
-        <li class="sidebar-item">
+        <li class="sidebar-item {{ end($url)=='Management'?'active':'' }}">
             <a href="/{{ user()->role }}/Management" class='sidebar-link'>
                 <i class="fas fa-building"></i>
                 <span>Company Management</span>
             </a>
         </li>
 
-        <li class="sidebar-item  has-sub">
+        <li class="sidebar-item  has-sub {{ end($url)=='See_All_Job'?'active':(end($url)=='Post_Job'?'active':'') }}">
             <a href="#" class='sidebar-link'>
                 <i class="fas fa-address-card"></i>
                 <span>Job</span>
             </a>
-            <ul class="submenu ">
-                <li class="submenu-item ">
+            <ul class="submenu {{ end($url)=='See_All_Job'?'active':(end($url)=='Post_Job'?'active':'') }}">
+                <li class="submenu-item {{ end($url)=='See_All_Job'?'active':'' }}">
                     <a href="/company/See_All_Job">See All</a>
                 </li>
-                <li class="submenu-item ">
+                <li class="submenu-item {{ end($url)=='Post_Job'?'active':'' }}">
                     <a href="/company/Post_Job">Post New Job</a>
                 </li>
             </ul>
